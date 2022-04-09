fb3122249edb6a1c456a75008069d8fe88737941
maddy <maddykimborowicz@gmail.com>
2022-04-09 14:35:19 -0700

    readme and about me update

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -5,12 +5,18 @@ function AboutMe() {
   return (
     <section id="about-me">
       <h2>About Me</h2>
+<div class="about-me">
+      <img class="image" src={require("../Assets/Avatar.jpg")} />
       <p>
-       
-        I am currently enrolled in University of Washington full stack web developement program! I was born and raised in Seattle, Washington. Being surrounded by many tech companies and opportunities sparked an interest in learning to code. In my journey so far I have had hands on experience learning HTML, CSS, Javascript, API's, Sequelize, Node, Handlebars, MonoDB, and react. I am horrible at writing these so I will write a better one later.
-
-        </p>
-      
+        I am currently enrolled in University of Washington full stack web
+        developement program! I was born and raised in Seattle, Washington.
+        Being surrounded by many tech companies and opportunities sparked an
+        interest in learning to code. In my journey so far I have had hands on
+        experience learning HTML, CSS, Javascript, API's, Sequelize, Node,
+        Handlebars, MonoDB, and react. I am horrible at writing these so I will
+        write a better one later.
+      </p>
+      </div>
     </section>
   );
 }
